741fc1961574c45f218cd8e0f91ab16ed2700a97
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:20:00 -0700

    Create ipop.py

---
+++ ipop.py
@@ -0,0 +1,23 @@
+'''
+You are given a temperature in Celsius. You need to convert it to Fahrenheit.
+Input and Output Format
+Input Format:
+A single integer celsius.
+Output Format:
+A single line containing the temperature in Fahrenheit.
+To convert a temperature from Celsius to Fahrenheit, you use the following formula:
+Multiply the temperature in Celsius by 9/5.
+Add 32 to the result of step 1.
+Sample Input 1
+0
+Sample Output 1
+32.0
+Sample Input 2
+25
+Sample Output 2
+77.0
+Sample Input 3
+100
+Sample Output 3
+212.0
+'''
